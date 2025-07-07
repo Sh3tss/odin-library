892ed70cb29cac13d536b43f26e7945d8501478a
Sh3tss <gabrielggartist@gmail.com>
2025-07-07 17:42:02 -0300

    did the close modal when clicked the insertbook inside the form

--- script.js
+++ script.js
@@ -9,8 +9,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const newbtn = document.getElementById('newbookbtn');
     const modal = document.getElementById('modalOverlay');
+    const insertbtn = document.getElementById('insertbookbtn');
 
+    //make the form openup when the new book get clicked
     newbtn.addEventListener('click', () => {
         modal.classList.add('active');
+    })
+    //make the form close after click the insert button inside the form
+    insertbtn.addEventListener('click', () => {
+        modal.classList.remove('active');
     })
 })
