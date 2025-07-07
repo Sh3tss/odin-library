aa3568bb661cf06ab158c0a1d4c1cf436ce9386e
Sh3tss <gabrielggartist@gmail.com>
2025-07-07 18:06:59 -0300

    create the variables to get the data from the form

--- script.js
+++ script.js
@@ -10,13 +10,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const newbtn = document.getElementById('newbookbtn');
     const modal = document.getElementById('modalOverlay');
     const insertbtn = document.getElementById('insertbookbtn');
+    const closeModalbtn = document.getElementById('closeModalbtn');
+    const author = document.getElementById('author');
+    const title = document.getElementById('title');
+    const pages = document.getElementById('pages');
 
     //make the form openup when the new book get clicked
     newbtn.addEventListener('click', () => {
         modal.classList.add('active');
     })
     //make the form close after click the insert button inside the form
-    insertbtn.addEventListener('click', () => {
+    insertbtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        const authorValue = author.value;
+        const titleValue = title.value;
+        const pagesValue = pages.value;
+        console.log('Autor:', authorValue);
+        console.log('Título:', titleValue);
+        console.log('Páginas:', pagesValue);
+        modal.classList.remove('active');
+    })
+    closeModalbtn.addEventListener('click', () => {
         modal.classList.remove('active');
     })
 })
